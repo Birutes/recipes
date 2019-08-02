2d134d7d7d960458432d1a822723e23ebf381f04
Birute <birutepaplauskaite@yahoo.com>
2019-08-02 16:01:48 +0300

    Mounted

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
+    context: path.join(__dirname, 'src'),
     entry: './index.js',
     output: {
-        path: __dirname,
+        path: path.resolve(__dirname, 'dist'),
         filename: "./bundle.js"
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
                 loader: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:"./index.html",
+            inject:"body"
+        })
+    ]
 }
